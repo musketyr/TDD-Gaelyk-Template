652cac60694d35dbd68f985b9f64cb064327f6ac
Vladimír Oraný <vladimir@orany.cz>
2011-03-17 13:15:11 +0100

    made build.d hidden

--- build.groovy
+++ build.groovy
@@ -1,12 +1,12 @@
 AntBuilder ant = []
-GroovyScriptEngine gse = [["build.d"] as String[]]
+GroovyScriptEngine gse = [[".build.d"] as String[]]
 Binding binding = []
 binding.ant = ant
 binding.args = args
 
 
 ant.sequential {
-	new File("build.d").list().findAll{it ==~ /S\d{3}.*\.groovy/}.sort().each{
+	new File(".build.d").list().findAll{it ==~ /S\d{3}.*\.groovy/}.sort().each{
 		echo "Running part $it"
 		gse.run(it, binding)
 	}
